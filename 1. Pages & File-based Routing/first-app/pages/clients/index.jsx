fa4c827afe020aa27fb28eba90079ace343fae58
piotr-skolorzynski <p.skolorzynski@gmail.com>
2022-09-08 14:41:24 +0200

    navigate to dynamic routes

--- 1. Pages & File-based Routing/first-app/pages/clients/index.jsx
+++ 1. Pages & File-based Routing/first-app/pages/clients/index.jsx
@@ -1,7 +1,29 @@
+import Link from 'next/link';
+
 const ClientsPage = () => {
+  const clients = [
+    {
+      id: 'piotr',
+      name: 'Piotr',
+    },
+    {
+      id: 'magda',
+      name: 'Magdalena',
+    },
+  ];
+
   return (
     <div>
       <h1>The Clients Page</h1>
+      <ul>
+        {clients.map((client) => {
+          return (
+            <li key={client.id}>
+              <Link href={`/clients/${client.id}`}>{client.name}</Link>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 };
