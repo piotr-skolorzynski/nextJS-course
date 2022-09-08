7a4b4b6db800f2f42c9fca8f8fecbd9b32256cb7
piotr-skolorzynski <p.skolorzynski@gmail.com>
2022-09-08 12:23:22 +0200

    initial commit for Pages & File-based Routing part

---
+++ 1. Pages & File-based Routing/first-app/pages/index.jsx
@@ -0,0 +1,9 @@
+import styles from '../styles/Home.module.css';
+
+const HomePage = () => {
+  <div className={styles.container}>
+    <h1>Hello Next World!</h1>
+  </div>;
+};
+
+export default HomePage;
