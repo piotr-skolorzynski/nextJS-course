abcfabbd711dad3ff6fec880bbffb16d2386c1a3
piotr-skolorzynski <p.skolorzynski@gmail.com>
2022-09-08 13:12:37 +0200

    add first page

--- 1. Pages & File-based Routing/first-app/pages/index.jsx
+++ 1. Pages & File-based Routing/first-app/pages/index.jsx
@@ -1,9 +1,9 @@
-import styles from '../styles/Home.module.css';
-
 const HomePage = () => {
-  <div className={styles.container}>
-    <h1>Hello Next World!</h1>
-  </div>;
+  return (
+    <div>
+      <h1>The Home Page</h1>
+    </div>
+  );
 };
 
 export default HomePage;
