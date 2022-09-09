25727ccff6a71b1cda3f06db8871a29a8559eba2
piotr-skolorzynski <p.skolorzynski@gmail.com>
2022-09-09 08:24:09 +0200

    navigating programmatically

--- 1. Pages & File-based Routing/first-app/pages/clients/[id]/index.jsx
+++ 1. Pages & File-based Routing/first-app/pages/clients/[id]/index.jsx
@@ -1,7 +1,21 @@
+import { useRouter } from 'next/router';
+
 const ClientProjectsPage = () => {
+  const router = useRouter();
+
+  const loadProjectHandler = () => {
+    // router.push('/clients/skolo/projecta');
+    //alternative from Next.js
+    router.push({
+      pathname: '/clients/[id]/[clientprojectid]',
+      query: { id: 'skolo', clientprojectid: 'projecta' },
+    });
+  };
+
   return (
     <div>
-      <h1>The Client Projects Page</h1>
+      <h1>The Projects of a Given Client</h1>
+      <button onClick={loadProjectHandler}>Load Project A</button>
     </div>
   );
 };
