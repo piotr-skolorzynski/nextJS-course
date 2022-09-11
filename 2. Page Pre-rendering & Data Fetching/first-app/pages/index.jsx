e9e2c7a869e52159ceb6c3693b82c6d609079f58
piotr-skolorzynski <p.skolorzynski@gmail.com>
2022-09-11 11:03:00 +0200

    run server side code and use file system

--- 2. Page Pre-rendering & Data Fetching/first-app/pages/index.jsx
+++ 2. Page Pre-rendering & Data Fetching/first-app/pages/index.jsx
@@ -1,7 +1,14 @@
+import fs from 'fs/promises';
+import path from 'path';
+
 export const getStaticProps = async () => {
+  const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
+
   return {
     props: {
-      products: [{ id: 1, title: 'Product 1' }],
+      products: data.products,
     },
   };
 };
