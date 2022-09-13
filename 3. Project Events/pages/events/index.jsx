2877563ed965ed6390746a3f1d16660bc0475651
piotr-skolorzynski <p.skolorzynski@gmail.com>
2022-09-13 13:11:03 +0200

    navigate to the filtered events programmatically

--- 3. Project Events/pages/events/index.jsx
+++ 3. Project Events/pages/events/index.jsx
@@ -1,13 +1,21 @@
+import { useRouter } from 'next/router';
 import EventList from '../../components/events/event-list';
 import EventsSearch from '../../components/events/events-search';
 import { getAllEvents } from '../../dummy-data';
 
 const AllEventsPage = () => {
   const events = getAllEvents();
+  const router = useRouter();
+
+  const findEventsHandler = (year, month) => {
+    const fullPath = `/events/${year}/${month}`;
+
+    router.push(fullPath);
+  };
 
   return (
     <>
-      <EventsSearch />
+      <EventsSearch onSearch={findEventsHandler} />
       <EventList items={events} />
     </>
   );
