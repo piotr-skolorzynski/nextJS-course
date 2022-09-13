a52dc3b470ce8d93c07f2a04e0c7284dc8433095
piotr-skolorzynski <p.skolorzynski@gmail.com>
2022-09-13 08:30:46 +0200

    add event detail page - dynamic routes

---
+++ 3. Project Events/components/event-detail/logistics-item.jsx
@@ -0,0 +1,14 @@
+import classes from './logistics-item.module.css';
+
+const LogisticsItem = ({ children, icon: Icon }) => {
+  return (
+    <li className={classes.item}>
+      <span className={classes.icon}>
+        <Icon />
+      </span>
+      <span className={classes.content}>{children}</span>
+    </li>
+  );
+};
+
+export default LogisticsItem;
