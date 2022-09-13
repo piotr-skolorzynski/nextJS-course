e5aec42cd0a3a73cbde096b85266d213eadcdcea
piotr-skolorzynski <p.skolorzynski@gmail.com>
2022-09-13 09:45:12 +0200

    add filter form for filtering events

--- 3. Project Events/pages/events/index.jsx
+++ 3. Project Events/pages/events/index.jsx
@@ -1,10 +1,16 @@
 import EventList from '../../components/events/event-list';
+import EventsSearch from '../../components/events/events-search';
 import { getAllEvents } from '../../dummy-data';
 
 const AllEventsPage = () => {
   const events = getAllEvents();
 
-  return <EventList items={events} />;
+  return (
+    <>
+      <EventsSearch />
+      <EventList items={events} />
+    </>
+  );
 };
 
 export default AllEventsPage;
