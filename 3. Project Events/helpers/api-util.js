fd797b2b90b779ad626b1a8f2c28fb603cc6f1f2
piotr-skolorzynski <p.skolorzynski@gmail.com>
2022-09-15 15:56:04 +0200

    load data and paths for dynamic pages

--- 3. Project Events/helpers/api-util.js
+++ 3. Project Events/helpers/api-util.js
@@ -17,3 +17,9 @@ export const getFeaturedEvents = async () => {
 
     return allEvents.filter((event) => event.isFeatured);
 }
+
+export const getEventById = async (id) => {
+    const allEvents = await getAllEvents();
+
+    return allEvents.find((event) => event.id === id);
+}
--- 3. Project Events/pages/events/[eventId].jsx
+++ 3. Project Events/pages/events/[eventId].jsx
@@ -1,16 +1,11 @@
-import { useRouter } from 'next/router';
-import { getEventById } from '../../dummy-data';
+import { getEventById, getAllEvents } from '../../helpers/api-util';
 import EventSummary from '../../components/event-detail/event-summary';
 import EventLogistics from '../../components/event-detail/event-logistics';
 import EventContent from '../../components/event-detail/event-content';
 import ErrorAlert from '../../components/events/error-alert';
 import Button from '../../components/ui/button';
 
-const EventDetailPage = () => {
-  const router = useRouter();
-  const eventId = router.query.eventId;
-  const event = getEventById(eventId);
-
+const EventDetailPage = ({ event }) => {
   if (!event) {
     return (
       <>
@@ -38,6 +33,29 @@ const EventDetailPage = () => {
       </EventContent>
     </>
   );
+};
+
+export const getStaticProps = async (context) => {
+  const eventId = context.params.eventId;
+
+  const event = await getEventById(eventId);
+
+  return {
+    props: {
+      event,
+    },
+  };
+};
+
+export const getStaticPaths = async () => {
+  const events = await getAllEvents();
+
+  const paths = events.map((event) => ({ params: { eventId: event.id } }));
+
+  return {
+    paths: paths,
+    fallback: false,
+  };
 };
 
 export default EventDetailPage;
