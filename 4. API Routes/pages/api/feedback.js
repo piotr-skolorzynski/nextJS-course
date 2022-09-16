3d5210c34ff3d13eb438c121214c1552a9ce0bdf
piotr-skolorzynski <p.skolorzynski@gmail.com>
2022-09-16 14:11:49 +0200

    parse the incoming request and executing server-side code

---
+++ 4. API Routes/pages/api/feedback.js
@@ -0,0 +1,27 @@
+import fs from 'fs';
+import path from 'path';
+
+const handler = (req, res) => {
+    if (req.method === 'POST') {
+        const email = req.body.email;
+        const feedbackText = rew.body.text;
+
+        const newFeedback = {
+            id: new Date().toISOString(),
+            email: email,
+            text: feedbackText
+        }
+
+        //store that in a database or in a file
+        const filePath = path.join(process.cwd(), 'data', 'feedback.json');
+        const fileData = fs.readFileSync(filePath);
+        const data = JSON.parse(fileData);
+        data.push(newFeedback);
+        fs.writeFileSync(filePath, JSON.stringify(data));
+        return res.status(201).json({ message: 'Success!', feedback: newFeedback })
+    }
+    res.status(200).json({ message: "This works!" });
+
+}
+
+export default handler;
