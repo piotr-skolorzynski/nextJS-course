eba6d3dfc1d2a406f20293389550e3a0e00d990c
piotr-skolorzynski <p.skolorzynski@gmail.com>
2022-09-16 15:38:18 +0200

    use API ROUTES to get data

--- 4. API Routes/pages/index.jsx
+++ 4. API Routes/pages/index.jsx
@@ -1,8 +1,10 @@
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 
 const HomePage = () => {
   const emailInput = useRef();
   const feedbackInput = useRef();
+
+  const [feedbackItems, setFeedbackItems] = useState([]);
 
   const submitHandler = (e) => {
     e.preventDefault();
@@ -23,6 +25,12 @@ const HomePage = () => {
       .then((data) => console.log(data));
   };
 
+  const loadFeedbackHandler = () => {
+    fetch('/api/feedback')
+      .then((response) => response.json())
+      .then((data) => setFeedbackItems(data.feedback));
+  };
+
   return (
     <div>
       <h1>The Home Page</h1>
@@ -37,6 +45,13 @@ const HomePage = () => {
         </div>
         <button>Send Feedback</button>
       </form>
+      <hr />
+      <button onClick={loadFeedbackHandler}>Load Feedback</button>
+      <ul>
+        {feedbackItems.map((item) => (
+          <li key={item.id}>{item.text}</li>
+        ))}
+      </ul>
     </div>
   );
 };
