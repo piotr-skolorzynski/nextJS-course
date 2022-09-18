4e246557e5a9d5aa10a01d7d605f03fdc4c83381
piotr-skolorzynski <p.skolorzynski@gmail.com>
2022-09-18 21:27:30 +0200

    use API Routes for pre-rendered pages

--- 4. API Routes/pages/api/feedback.js
+++ 4. API Routes/pages/api/feedback.js
@@ -1,9 +1,9 @@
 import fs from 'fs';
 import path from 'path';
 
-const buildFeedbackPath = () => path.join(process.cwd(), 'data', 'feedback.json');
+export const buildFeedbackPath = () => path.join(process.cwd(), 'data', 'feedback.json');
 
-const extractFeedback = (filePath) => {
+export const extractFeedback = (filePath) => {
     const fileData = fs.readFileSync(filePath);
     const data = JSON.parse(fileData);
 
---
+++ 4. API Routes/pages/feedback/index.jsx
@@ -0,0 +1,24 @@
+import { buildFeedbackPath, extractFeedback } from '../api/feedback';
+
+const FeedbackPage = ({ feedbackItems }) => {
+  return (
+    <ul>
+      {feedbackItems.map((item) => (
+        <li key={item.id}>{item.text}</li>
+      ))}
+    </ul>
+  );
+};
+
+export const getStaticProps = async () => {
+  const filePath = buildFeedbackPath();
+  const data = extractFeedback(filePath);
+
+  return {
+    props: {
+      feedbackItems: data,
+    },
+  };
+};
+
+export default FeedbackPage;
