02194a25ecb3262ffb5859fe7368607424eec5ad
piotr-skolorzynski <p.skolorzynski@gmail.com>
2022-09-18 21:53:07 +0200

    create dynamic API Routes

--- 4. API Routes/pages/feedback/index.jsx
+++ 4. API Routes/pages/feedback/index.jsx
@@ -1,12 +1,29 @@
+import { useState } from 'react';
 import { buildFeedbackPath, extractFeedback } from '../api/feedback';
 
 const FeedbackPage = ({ feedbackItems }) => {
+  const [feedbackData, setFeedbackData] = useState(null);
+
+  const loadFeedbackHandler = (e, id) => {
+    fetch(`/api/${id}`)
+      .then((response) => response.json())
+      .then((data) => setFeedbackData(data.feedback));
+  };
+
   return (
-    <ul>
-      {feedbackItems.map((item) => (
-        <li key={item.id}>{item.text}</li>
-      ))}
-    </ul>
+    <>
+      {feedbackData && <p>{feedbackData.email}</p>}
+      <ul>
+        {feedbackItems.map((item) => (
+          <li key={item.id}>
+            {item.text}{' '}
+            <button onClick={(e) => loadFeedbackHandler(e, item.id)}>
+              Show Details
+            </button>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 };
 
